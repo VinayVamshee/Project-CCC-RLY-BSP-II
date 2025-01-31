a23b71988529a84e65bb73fc9a277499dca3c202
VinayVamshee <144655152+VinayVamshee@users.noreply.github.com>
2025-01-31 20:22:51 +0530

    Update Contact.js

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -58,7 +58,7 @@ export default function Contact() {
     });
 
     const GetContactProfile = async (id) => {
-        axios.get('http://localhost:3001/GetContactProfile/' + id)
+        axios.get('https://ccc-bsp-server.vercel.app/GetContactProfile/' + id)
             .then(result => setEditContact(result.data))
             .catch(error => console.log(error))
     }
@@ -66,7 +66,7 @@ export default function Contact() {
     const EditContactProfile = async (id, e) => {
         e.preventDefault();
         try {
-            axios.put("http://localhost:3001/EditContactProfile/" + id, { ...EditContact })
+            axios.put("https://ccc-bsp-server.vercel.app/EditContactProfile/" + id, { ...EditContact })
                 .then(result => {
                     console.log(result)
                     alert('Edited')
@@ -86,7 +86,7 @@ export default function Contact() {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        axios.post('http://localhost:3001/AddNewContact', contactData)
+        axios.post('https://ccc-bsp-server.vercel.app/AddNewContact', contactData)
             .then((response) => {
                 console.log(response.data);
                 alert('New Contact Added');
@@ -100,7 +100,7 @@ export default function Contact() {
     const [AllContacts, setAllContacts] = useState();
 
     useEffect(() => {
-        axios.get('http://localhost:3001/GetContacts')
+        axios.get('https://ccc-bsp-server.vercel.app/GetContacts')
             .then(async (result) => {
                 // await new Promise(resolve => setTimeout(resolve, 500));
                 setAllContacts(result.data)
@@ -111,7 +111,7 @@ export default function Contact() {
     const DeleteContact = async (id) => {
         const AskUser = window.confirm("Are you sure?")
         if (AskUser) {
-            axios.delete('http://localhost:3001/DeleteContact/' + id)
+            axios.delete('https://ccc-bsp-server.vercel.app/DeleteContact/' + id)
                 .then(result => {
                     console.log(result)
                     window.location.reload();
